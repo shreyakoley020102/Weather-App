c1f5c4cd962bda5e5ca8365204b267c1f3a6a447
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-06-09 13:25:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Server is up and running");
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=02e242ef9fee0c3a2d7c8330fa5c3049&units=metric";
+
+  https.get(url, function (response) {
+    console.log(response);
+  });
+
+  res.send("Server is running");
 });
 
 app.listen(3000, function () {
-  console.log("Server is running on port 3000");
+  console.log("Server is running at port 3000");
 });
