919ca7063f54ea7cdcb627dae2d8bcd5e27f5c1e
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-06-10 17:52:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.get("/", function (req, res) {
     "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=02e242ef9fee0c3a2d7c8330fa5c3049&units=metric";
 
   https.get(url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+    });
   });
 
   res.send("Server is running");
