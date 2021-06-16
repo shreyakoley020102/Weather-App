ba7edddd6d0177d767e045378fab524b0f92fb56
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-06-16 19:01:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,71 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const https = require("https");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=02e242ef9fee0c3a2d7c8330fa5c3049&units=metric";
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  https.get(url, function (response) {
-    console.log(response.statusCode);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lasttName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lasttName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us6.api.mailchimp.com/3.0/lists/361d4341fc";
+
+  const options = {
+    method: "POST",
+    auth: "shreya02:550f297056d7a883329337ec8266d31-us6",
+    //add 1 before -us6, basically 311
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
 
     response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      console.log(weatherData);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write("<p>The weather is currently " + weatherDescription + "<p>");
-      res.write(
-        "<h1>The temperature in Paris is " + temp + " degree Celsius.</h1>"
-      );
-      res.write("<img src=" + imageURL + ">");
-      res.send();
-      // const weatherDescription = weatherData.weather[0].description;
+      console.log(JSON.parse(data));
     });
   });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
-  console.log("Server is running at port 3000");
+  console.log("Server is running on port 3000");
 });
+
+//API KEY
+//c4b91a3708904074c7a78260e793232f-us6
+
+//List id
+//361d4341fc
